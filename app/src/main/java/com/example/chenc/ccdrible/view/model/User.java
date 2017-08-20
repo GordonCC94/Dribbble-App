7d5ecddf56175ea50b092a69c2a0a72822227cd5
GordonCC94 <gchen940215@gmail.com>
2017-08-20 16:24:51 -0500

    CC Dribbble

---
+++ app/src/main/java/com/example/chenc/ccdrible/view/model/User.java
@@ -0,0 +1,10 @@
+package com.example.chenc.ccdrible.view.model;
+
+/**
+ * Created by chenc on 2017/8/20.
+ */
+
+public class User {
+    public String name;
+    public String avatar_url;
+}
---
+++ app/src/main/java/com/example/chenc/ccdrible/view/view/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.chenc.ccdrible.view.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.chenc.ccdrible.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
