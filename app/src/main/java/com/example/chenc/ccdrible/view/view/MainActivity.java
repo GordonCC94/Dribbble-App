1192f1af9243c470ce2f3b6f6ad42cfbc40cb572
GordonCC94 <gchen940215@gmail.com>
2017-08-22 17:53:33 -0500

    ShotList UI

--- app/src/main/java/com/example/chenc/ccdrible/view/view/MainActivity.java
+++ app/src/main/java/com/example/chenc/ccdrible/view/view/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.chenc.ccdrible.R;
+import com.example.chenc.ccdrible.view.view.shot_list.ShotListFragment;
+
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.fragment_container, ShotListFragment.newInstance())
+                    .commit();
+        }
     }
 }
--- app/src/main/java/com/example/chenc/ccdrible/view/view/shot_list/ShotListFragment.java
+++ app/src/main/java/com/example/chenc/ccdrible/view/view/shot_list/ShotListFragment.java
@@ -27,41 +27,42 @@ import butterknife.ButterKnife;
  * Created by chenc on 2017/8/22.
  */
 
-public class ShotListFragment extends Fragment {
+public class ShotListFragment extends android.support.v4.app.Fragment {
+
     @BindView(R.id.recycler_view) RecyclerView recyclerView;
-    public static ShotListFragment newInstance(){
+
+    public static ShotListFragment newInstance() {
         return new ShotListFragment();
     }
 
-    @NonNull
+    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,
                              @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState){
-        View view=inflater.inflate(R.layout.fragment_recycler_view,container,false);
-        ButterKnife.bind(this,view);
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_recycler_view, container, false);
+        ButterKnife.bind(this, view);
         return view;
     }
-    @RequiresApi(api = Build.VERSION_CODES.M)
+
     @Override
-    public void onViewCreated(View view,@Nullable Bundle savedInstanceState){
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.addItemDecoration(new SpaceItemDecoration(
-                getResources().getDimensionPixelSize(R.dimen.spacing_medium)
-        ));
+                getResources().getDimensionPixelSize(R.dimen.spacing_medium)));
 
-        ShotListAdapter adapter=new ShotListAdapter(fakeData());
+        ShotListAdapter adapter = new ShotListAdapter(fakeData());
         recyclerView.setAdapter(adapter);
     }
 
-    private List<Shot> fakeData(){
-        List<Shot> shotList=new ArrayList<>();
-        Random random=new Random();
-        for(int i=0;i<20;i++){
-            Shot shot=new Shot();
-            shot.views_count=random.nextInt(10000);
-            shot.likes_count=random.nextInt(200);
-            shot.buckets_count=random.nextInt(50);
+    private List<Shot> fakeData() {
+        List<Shot> shotList = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 20; ++i) {
+            Shot shot = new Shot();
+            shot.views_count = random.nextInt(10000);
+            shot.likes_count = random.nextInt(200);
+            shot.buckets_count = random.nextInt(50);
             shotList.add(shot);
         }
         return shotList;
